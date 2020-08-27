877a96f142987a6178db52a1672d32862266f251
Arnab Ari <arnab.ari.ndp@gmail.com>
2020-08-28 01:57:59 +0530

    Natural language converter of date time added

--- Assignment2/DateTimeCalculator/src/com/sapient/datetime/DateCalculator.java
+++ Assignment2/DateTimeCalculator/src/com/sapient/datetime/DateCalculator.java
@@ -1,5 +1,7 @@
 package com.sapient.datetime;
 
+import java.time.Month;
+
 public class DateCalculator {
 	public void subtractDate(MyDate d1, MyDate d2) {	
 		long difference = (d2.getEpochSeconds() - d1.getEpochSeconds())/86400;
@@ -14,4 +16,84 @@ public class DateCalculator {
 	public MyDate subtractDaysFrom(MyDate d,long n) {
 		return addDaysTo(d,-n);
 	}
+	
+	public MyDate today() {
+		return new MyDate();
+	}
+	
+	public MyDate tomorrow() {
+		return addDaysTo(new MyDate(),1);
+	}
+	
+	public MyDate dayAfterTomorrow() {
+		return addDaysTo(new MyDate(),2);
+	}
+	
+	public MyDate yesterday() {
+		return subtractDaysFrom(new MyDate(),1);
+	}
+	
+	public MyDate dayBeforeYesterday() {
+		return subtractDaysFrom(new MyDate(),2);
+	}
+	
+	public String nextMonth() {
+		return Month.of(new MyDate().date.getMonthValue() + 1).toString();	
+	}
+	
+	public String lastMonth() {
+		return Month.of(new MyDate().date.getMonthValue() - 1).toString();	
+	}
+	
+	public int nextYear() {
+		return (new MyDate().date.getYear() + 1);
+	}
+	
+	public int lastYear() {
+		return (new MyDate().date.getYear() - 1);
+	}
+	
+	void performOperation(int option) {
+		switch(option) {
+			case 1:
+				System.out.println(today());
+				break;
+				
+			case 2:
+				System.out.println(tomorrow());
+				break;
+			
+			case 3:
+				System.out.println(dayAfterTomorrow());
+				break;
+			
+			case 4:
+				System.out.println(yesterday());
+				break;
+				
+			case 5:
+				System.out.println(dayBeforeYesterday());
+				break;
+				
+			case 6:
+				System.out.println(nextMonth());
+				break;
+				
+			case 7:
+				System.out.println(lastMonth());
+				break;
+				
+			case 8:
+				System.out.println(nextYear());
+				break;
+				
+			case 9:
+				System.out.println(lastYear());
+				break;
+			
+			default:
+				System.out.println("Error: INVALID OPTION !!!!");
+		}
+	}
 }
+//   Last week, Previous week, Next week,  Month after, Month before, n weeks from now, n days from now, n months from now, n years from now, n days earlier, n weeks earlier, n months earlier, n years earlier
---
+++ Assignment2/DateTimeCalculator/src/com/sapient/datetime/DateCalculatorMenu.java
@@ -0,0 +1,16 @@
+package com.sapient.datetime;
+
+public class DateCalculatorMenu {
+	public void getOptions() {
+		System.out.println("===SELECT AN OPERATION FROM THE MENU===");
+		System.out.println("1. Today");
+		System.out.println("2. Tomorrow");
+		System.out.println("3. Day after tomorrow");
+		System.out.println("4. Yesterday");
+		System.out.println("5. Day before yesterday");
+		System.out.println("6. Next month");
+		System.out.println("7. Last month");
+		System.out.println("8. Next year");
+		System.out.println("9. Last year");
+	}
+}
